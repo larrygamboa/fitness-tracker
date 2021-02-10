44543a88b8fcad2d014889e852708109f15c2441
Larry Gamboa <techmedia.lg@gmail.com>
2021-02-09 18:51:13 -0800

    update mongodb connection on seed.js

--- seeders/seed.js
+++ seeders/seed.js
@@ -3,8 +3,9 @@ let db = require("../models");
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 let workoutSeed = [
